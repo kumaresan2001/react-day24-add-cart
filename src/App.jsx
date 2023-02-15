816d14c542e97ab1f4bd381d8519cdbe3d9d7873
kumaresan2001 <kumaresancrseven@gmail.com>
2023-02-15 15:02:38 +0530

    add cart-day24

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+
+import data from "./data/data";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Navigation from "./components/Navigation";
+import Section from "./components/Section";
+
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const getItem = (event) => {
+    //console.log(event);
+    if (event.target.innerText === "Add to Cart") {
+      addItem(event);
+    } else if (event.target.innerText === "Remove from Cart") {
+      removeItem(event);
+    }
+  };
+
+  function addItem(event) {
+    setCount(count + 1);
+    event.target.innerText = "Remove from Cart";
+    event.target.classList.value = "btn btn-outline-danger mt-auto";
+  }
+
+  function removeItem(event) {
+    setCount(count - 1);
+    event.target.innerText = "Add to Cart";
+    event.target.classList.value = "btn btn-outline-dark mt-auto";
+  }
+
+  return (
+    <>
+      <Navigation count={count} />
+      <Header />
+      <Section data={data} getItem={getItem} />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
